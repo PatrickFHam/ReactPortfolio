bb71b72c56fc6c57374f46abc118ed9ccd350ef8
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-05-20 17:31:50 -0400

    cards, their info and images all work, next is formatting

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// import SpeedRunImage from './images/speedrun-bg-sonic.png'
+
 import {
   SpeedRunImage,
   WeatherDashboardImage,
@@ -7,41 +10,53 @@ import {
   PasswordGeneratorImage
 } from '../pages/images'
 
+import { Card, Button } from 'react-bootstrap'
+
 const shownProjects = [
   {
+    id: 1,
     title: "SpeedRun Spectator",
     subtitle: "3rd-Party-APIs / Javascript / Bootstrap",
     githubLink: "https://github.com/PatrickFHam/SpeedRunSpectator",
     liveLink: "https://patrickfham.github.io/SpeedRunSpectator/",
-    image: {SpeedRunImage}
+    image: SpeedRunImage,
+    cardSize: 'card-big'
   },
   {
+    id: 2,
     title: "Weather Dashboard",
     subtitle: "3rd-Party-APIs / Javascript / Bootstrap",
     githubLink: "https://github.com/PatrickFHam/WeatherDashboard",
     liveLink: "https://patrickfham.github.io/WeatherDashboard/",
-    image: {WeatherDashboardImage}
+    image: WeatherDashboardImage,
+    cardSize: 'card-big'
   },
   {
+    id: 3,
     title: "Workday Planner",
     subtitle: "Javascript / Bootstrap",
     githubLink: "https://github.com/PatrickFHam/WorkdayScheduler",
     liveLink: "https://patrickfham.github.io/WorkdayScheduler/",
-    image: {WorkdayPlannerImage}
+    image: WorkdayPlannerImage,
+    cardSize: 'card-med'
   },
   {
+    id: 4,
     title: "Code Quiz",
     subtitle: "Javascript / CSS",
     githubLink: "https://github.com/PatrickFHam/CodeQuiz",
     liveLink: "https://patrickfham.github.io/CodeQuiz",
-    image: {CodeQuizImage}
+    image: CodeQuizImage,
+    cardSize: 'card-med'
   },
   {
+    id: 5,
     title: "Password Generator",
     subtitle: "Javascript",
     githubLink: "https://github.com/PatrickFHam/PasswordGenerator",
     liveLink: "https://patrickfham.github.io/PasswordGenerator/",
-    image: {PasswordGeneratorImage}
+    image: PasswordGeneratorImage,
+    cardSize: 'card-small'
   },
 ]
 
@@ -61,6 +76,22 @@ export default function Portfolio() {
         vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
         fames ac ante ipsum primis in faucibus.
       </p>
+      <section id="allthecards">
+      {shownProjects.map(item => (
+        <Card key={item.id} style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={item.image} />
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text>
+          {item.subtitle}
+          </Card.Text>
+          <Button href={item.githubLink} target='blank' variant="primary">See on GitHub</Button>
+          <Button href={item.liveLink} target='blank' variant="success">See it LIVE</Button>
+        </Card.Body>
+      </Card>
+      ))}
+        
+      </section>
     </div>
   );
 }
