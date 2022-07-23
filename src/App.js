829c1b32844495830a5aeed50e8e8a97022215b2
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-07-23 15:44:50 -0400

    removed bootstrap reference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import { NextUIProvider } from '@nextui-org/react';
 
 const App = () => 
